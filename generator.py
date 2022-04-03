b832387cb7fa2f6af695bcf6dec8cc41f21b8b25
Leonardo Zanotti <leonardojosezanotti@gmail.com>
2022-04-03 13:29:50 -0300

    first commit

---
+++ generator.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+states = [
+    'Acre',
+    'Alagoas',
+    'Amapá',
+    'Amazonas',
+    'Bahia',
+    'Ceará',
+    'Espírito Santo',
+    'Goiás',
+    'Maranhão',
+    'Mato Grosso',
+    'Mato Grosso do Sul',
+    'Minas Gerais',
+    'Pará',
+    'Paraíba',
+    'Paraná',
+    'Pernambuco',
+    'Piauí',
+    'Rio de Janeiro',
+    'Rio Grande do Norte',
+    'Rio Grande do Sul',
+    'Rondônia',
+    'Roraima',
+    'Santa Catarina',
+    'São Paulo',
+    'Sergipe',
+    'Tocantins',
+    'Distrito Federal'
+]
+
+ufs = [
+    'AC',
+    'AL',
+    'AP',
+    'AM',
+    'BA',
+    'CE',
+    'ES',
+    'GO',
+    'MA',
+    'MT',
+    'MS',
+    'MG',
+    'PA',
+    'PB',
+    'PR',
+    'PE',
+    'PI',
+    'RJ',
+    'RN',
+    'RS',
+    'RO',
+    'RR',
+    'SC',
+    'SP',
+    'SE',
+    'TO',
+    'DF'
+]
+
+for i, state in enumerate(states):
+    print("INSERT INTO tb_estado(nome_estado, sigla_estado)\nVALUES ('{}', '{}');\n".format(
+        state, ufs[i]))
+
+
+cities = []
+
+for i, city in enumerate(cities):
+    print("INSERT INTO tb_cidade(nome_cidade, id_estado)V\nVALUES ('{}', '{}');\n".format(
+        city, i % 10 + 1))
